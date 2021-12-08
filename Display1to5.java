bd47e7cddf7a32f4817fca80b3c11b5a038646fc
Dhavalsinh Dhanaji Patil <76774786+dhavalsinhpatil@users.noreply.github.com>
2021-12-08 19:14:23 +0530

    Create Display1to5.java

---
+++ Display1to5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.security.Principal;
+import java.util.*;
+
+// Display 1 to 5 on screen.
+
+class program3
+{
+
+    public static void Display()
+    {
+        for(int i=1; i<=5; i++)
+        {
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) 
+    {
+        Display();
+    }
+}
